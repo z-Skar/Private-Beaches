a91aaff5d27883245f7b724c6a6a4324fb796ef4
z-Skar <oscar.borfotina.dobrovan@gmail.com>
2024-12-09 10:14:06 +0000

    Implementação dos dados no ThreeColSlider

--- src/components/cards/ThreeColSlider.js
+++ src/components/cards/ThreeColSlider.js
@@ -54,8 +54,8 @@ const Rating = tw.span`ml-2 font-bold`;
 
 const Description = tw.p`text-sm leading-loose mt-2 sm:mt-4`;
 
-const SecondaryInfoContainer = tw.div`flex flex-col sm:flex-row mt-2 sm:mt-4`;
-const IconWithText = tw.div`flex items-center mr-6 my-2 sm:my-0`;
+const SecondaryInfoContainer = tw.div`flex flex-col justify-between sm:flex-row mt-2 sm:mt-4`;
+const IconWithText = tw.div`flex items-center sm:my-0`;
 const IconContainer = styled.div`
   ${tw`inline-block rounded-full p-2 bg-gray-700 text-gray-100`}
   svg {
@@ -95,8 +95,13 @@ export default () => {
       try {
         const response = await fetch('http://localhost:5000/beaches')
         const data = await response.json();
-        setBeaches(data);
-        console.log(data);
+        const updatedBeaches = data.map(beach => ({
+          ...beach,
+          PICTURE: `http://localhost:5000${beach.PICTURE}`
+        }));
+        setBeaches(updatedBeaches);
+        console.log(beaches);
+        console.log(beaches[0]);
       } catch (error) {
         console.log('Fetch Error: ', error);
       };
@@ -107,23 +112,23 @@ export default () => {
   /* Change this according to your needs */
   const cards = [
     {
-      imageSrc: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: beaches[0].LOCATION,
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Rome, Italy",
-      pricingText: "EUR 39/",
-      rating: "4.8",
+      imageSrc: "https://images.unsplash.com/photo-1549294413-26f195200c16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
+      title: beaches[0].BEACH_NAME,
+      description: beaches[0].DESCRIPTION,
+      locationText: `${beaches[0].CITY_LOCATION}, ${beaches[0].COUNTRY_LOCATION}`,
+      pricingText: `EUR ${beaches[0].RESERVATION_COST}`,
+      rating: beaches[0].SCORE,
     },
     {
       imageSrc: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Soho Paradise",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Ibiza, Spain",
-      pricingText: "EUR 50/Dia",
-      rating: 4.9,
+      title: beaches[0].BEACH_NAME,
+      description: beaches[0].DESCRIPTION,
+      locationText: `${beaches[0].CITY_LOCATION}, ${beaches[0].COUNTRY_LOCATION}`,
+      pricingText: `EUR ${beaches[0].RESERVATION_COST}`,
+      rating: beaches[0].SCORE,
     },
     {
-      imageSrc: "https://images.unsplash.com/photo-1549294413-26f195200c16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
+      imageSrc: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
       title: "Hotel Baja",
       description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
       locationText: "Palo Alto, CA",
