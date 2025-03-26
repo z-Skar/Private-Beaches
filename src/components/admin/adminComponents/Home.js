73a53dd97c8e06775a981d78e22e6d2e1b568671
z-Skar <oscar.borfotina.dobrovan@gmail.com>
2025-03-26 15:26:25 +0000

    Atualizar código

--- src/components/admin/adminComponents/Home.js
+++ src/components/admin/adminComponents/Home.js
@@ -2,9 +2,14 @@ import Box from '@mui/material/Box';
 import Card from '@mui/joy/Card';
 import { useAdminData } from "contexts/AdminDataContext";
 import { Typography } from '@mui/joy';
+import { useEffect } from 'react';
 
 export default function Home() {
-    const { adminData } = useAdminData();
+    const { adminData, setAdminEntity } = useAdminData();
+    
+    useEffect(() => {
+        setAdminEntity('home');
+    }, []);
 
     const DEFAULT_MESSAGE = 'A carregar dados...';
     const TOTAL_PROFIT = adminData?.[0]?.['Lucro Total'] ?? DEFAULT_MESSAGE;
@@ -12,22 +17,34 @@ export default function Home() {
     const PENDENT_LIFEGUARDS = adminData?.[0]?.['Salva-vidas pendentes'] ?? DEFAULT_MESSAGE;
 
     const CARD_STYLE = {
-        backgroundColor: '#F0F4F8'
+        backgroundColor: '#F0F4F8',
+        width: '18rem',
+        height: '10rem',
     };
     
+    const DATA_STYLE = {
+        fontSize: '1.5rem',
+        color: '#FF5A00',
+        marginBottom: '-1rem'
+    };
+
     return (
-        <Box sx={{ padding: '10px', borderRadius: '10px', display: 'flex', gap: '1rem' }}>
+        <Box sx={{ padding: '10px', borderRadius: '10px', display: 'flex', gap: '1rem', flexWrap: 'wrap', justifyContent: 'space-between' }}>
             <Card sx={CARD_STYLE}>
-                <Typography variant="h2">Lucro Total</Typography>
-                {TOTAL_PROFIT}
+            <Typography variant="h2" sx={DATA_STYLE}>{NUMBER_OF_CLIENTS} mil</Typography>
+                Número Total de Clientes
             </Card>
             <Card sx={CARD_STYLE}>
-            <Typography variant="h2">Número Total de Clientes</Typography>
-                {NUMBER_OF_CLIENTS}
+                <Typography variant="h2" sx={DATA_STYLE}>{TOTAL_PROFIT}</Typography>
+                Lucro Total
             </Card>
             <Card sx={CARD_STYLE}>
-            <Typography variant="h2">Candidaturas de Salva-vidas pendentes</Typography>
-                {PENDENT_LIFEGUARDS}
+            <Typography variant="h2" sx={DATA_STYLE}>{PENDENT_LIFEGUARDS}</Typography>
+                Candidaturas de Salva-vidas pendentes
+            </Card>
+            <Card sx={CARD_STYLE}>
+                <Typography variant="h2" sx={DATA_STYLE}>10</Typography>
+                Praias
             </Card>
         </Box>
     );
